a490a6244c0eb62e20cb40a5499504d63f5fe66c
nasir-beep <nash144@live.com>
2026-01-07 16:46:01 +0200

    Add changes to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,13 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
+      <WelcomeMessage />
+
       <Header />
       <MainContent />
 
