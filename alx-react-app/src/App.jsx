51e795b473924ae17817ebbe65ef16d0155cade1
nasir-beep <nash144@live.com>
2026-01-07 16:32:42 +0200

    Add updates to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,20 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+
+      <UserProfile 
+        name="Ntokozo" 
+        age="30" 
+        bio="Student learning React and Web development" 
+      />
+
       <Footer />
     </div>
   );
