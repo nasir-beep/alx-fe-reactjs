5fd7b0292c5ca29f158e1c0cd97b7eb828530f3e
nasir-beep <nash144@live.com>
2026-01-07 16:11:02 +0200

    Add WecomeMessage.jsx file

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from "./components/WelcomeMessage"; function WelcomeMessage() {
+function WelcomeMessage() {
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
