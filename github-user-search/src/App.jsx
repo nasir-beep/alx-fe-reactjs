41ba1df79fee6463100e9e513433e044c547ff0b
nasir-beep <nash144@live.com>
2026-02-02 09:23:58 +0200

    Add components and services folders and initial file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,7 @@
-// src/App.jsx
-import React from "react";
-import SearchBar from "./components/Search";
-
 function App() {
   return (
-    <div className="app-container">
+    <div>
       <h1>GitHub User Search</h1>
-      <SearchBar />
     </div>
   );
 }
