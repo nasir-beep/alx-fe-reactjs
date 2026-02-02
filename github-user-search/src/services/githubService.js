23146c0552c8be3682ac61e7ff024a0d5167dde7
nasir-beep <nash144@live.com>
2026-02-02 12:09:51 +0200

    Add updated githubService file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
+export const fetchAdvancedUserData = async ({ username, location, minRepos }) => {
+  let query = username || "";
+
+  if (location) {
+    query += `+location:${location}`;
+  }
+
+  if (minRepos) {
+    query += `+repos:>=${minRepos}`;
+  }
+
   const response = await axios.get(
-    `https://api.github.com/users/${username}`
+    `https://api.github.com/search/users?q=${query}`
   );
+
   return response.data;
 };
