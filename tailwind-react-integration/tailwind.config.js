5e96787e8312610a858ddd48e1718af7a6cbef35
nasir-beep <nash144@live.com>
2026-02-08 09:19:05 +0200

    Add updates to tailwind.config.js file

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ✅ Tailwind plugin
+import tailwindcss from '@tailwindcss/vite' // ✅ Add this
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // ✅ Enable Tailwind
+    tailwindcss(), // ✅ Add this
   ],
 })
