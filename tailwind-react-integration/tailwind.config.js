ced317284f27afbd76314bf85d9f9353c0c4d708
nasir-beep <nash144@live.com>
2026-02-08 09:06:31 +0200

    Add Vite.config.js file

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Tailwind plugin
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ Enable Tailwind
+  ],
+})
