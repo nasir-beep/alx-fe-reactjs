144bef31f5a270283a489074c51bbd3911c56bfb
nasir-beep <nash144@live.com>
2026-02-22 21:43:29 +0200

    Add updates to FormikForm.jsx file

---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,60 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikForm() {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    alert("Registration successful!");
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <h2>Formik Registration Form</h2>
+
+        <div>
+          <label>Username:</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+        </div>
+
+        <div>
+          <label>Email:</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+        </div>
+
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default FormikForm;
