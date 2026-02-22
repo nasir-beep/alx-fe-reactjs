29397bbf3a9dc060165988f29dbd96751e87384e
nasir-beep <nash144@live.com>
2026-02-22 22:50:19 +0200

    Add Profile.jsx to components

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+
+      <nav>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+
+      <hr />
+
+      {/* Nested routes render here */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
