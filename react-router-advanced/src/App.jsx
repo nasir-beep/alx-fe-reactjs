3dc70ed09e2e15c340973eb3369dda16cc1139fd
nasir-beep <nash144@live.com>
2026-02-22 23:40:50 +0200

    Add upates to App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import RegistrationForm from "./components/RegistrationForm";
 import PostsComponent from "./components/PostsComponent";
-import BlogPost from "./components/BlogPost"; // ✅ BlogPost import
+import BlogPost from "./components/BlogPost";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute"; import
 
-// Create QueryClient instance
 const queryClient = new QueryClient();
 
 function App() {
@@ -15,7 +17,17 @@ function App() {
         <Routes>
           <Route path="/" element={<RegistrationForm />} />
           <Route path="/posts" element={<PostsComponent />} />
-          <Route path="/blog/:id" element={<BlogPost />} /> {/* ✅ /blog/:id route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+
+          {/* ✅ /profile route protected */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </QueryClientProvider>
