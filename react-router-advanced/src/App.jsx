35e8ecab2c9d1675b8bc1db34b9917a6fd11ba18
nasir-beep <nash144@live.com>
2026-02-22 22:39:18 +0200

    Add updates to App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,49 +1,13 @@
-import { Routes, Route, Link } from "react-router-dom"
-import { useState } from "react"
-
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import ProfileDetails from "./pages/ProfileDetails"
-import ProfileSettings from "./pages/ProfileSettings"
-import Login from "./pages/Login"
-import BlogPost from "./pages/BlogPost"
-import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/1">Blog Post 1</Link> |{" "}
-        <Link to="/login">Login</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-
-        {/* Protected Route */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          {/* Nested Routes */}
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-
-        {/* Dynamic Route */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+    	<Routes>
+      	    <Route path="/" element={<Home />} />
+    	</Routes>
+    <BrowserRouter>
   )
 }
 
